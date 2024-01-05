e7e78cb0025b940bcb529681b26dd91a284972b6
rde_armas <robertodearmas.de@gmail.com>
2024-01-04 21:25:31 -0300

    Add screens initials

---
+++ cainsi-mobile/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './src/navigation/Navigation';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Navigation/>
+    </NavigationContainer>
+  );
+}
---
+++ cainsi-mobile/src/navigation/Navigation.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Icon from "react-native-vector-icons/Ionicons"
+import HomeScreen from '../screens/HomeScreen'
+import SettingsScreen from '../screens/SettingsScreen'
+import ReportScreen from '../screens/ReportScreen'
+import ReportsQueueScreen from '../screens/ReportsQueueScreen'
+
+
+const Tab = createBottomTabNavigator();
+
+export default function Navigation() {
+  return (
+    <Tab.Navigator>
+        <Tab.Screen name='Home' component={HomeScreen} options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ color, size }) => <Icon name="home" color={color} size={size} />,
+        }} />
+        <Tab.Screen name='Report' component={ReportScreen} options={{
+            tabBarLabel: "Report",
+            tabBarIcon: ({ color, size }) => <Icon name="document" color={color} size={size} />,
+            
+        }}/>
+        <Tab.Screen name='ReportsQueue' component={ReportsQueueScreen} options={{
+            tabBarLabel: "ReportsQueue",
+            tabBarIcon: ({ color, size }) => <Icon name="albums" color={color} size={size} />,
+        }} />
+        <Tab.Screen name='Settings' component={SettingsScreen} options={{
+            tabBarLabel: "Settings",
+            tabBarIcon: ({ color, size }) => <Icon name="settings" color={color} size={size} />,
+        
+        }}/>
+    </Tab.Navigator>
+  )
+}
