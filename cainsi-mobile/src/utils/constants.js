64b6e7adb27ca9d191d2f467262b41cf4fc4a582
rde_armas <robertodearmas.de@gmail.com>
2024-03-04 18:46:25 -0300

    Agrega los nuevos inconos

--- cainsi-mobile/src/utils/constants.js
+++ cainsi-mobile/src/utils/constants.js
@@ -23,41 +23,41 @@ const initialInputs = {
     empty:{},
 };
 
-const envolventes = [
+let envolventes = [
     { id: '1', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5, 6]], 
-        ['Casquete', ['Inferior','Superior']]
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5]], 
+        ['Casquete', ['Derecho','Izquierdo']]
         ], source: require('../assets/scheme/envolventes/1.png') },
     { id: '2', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4]], 
-        ['Casquete', ['Inferior','Superior']]
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F'], [1, 2, 3, 4, 5]], 
+        ['Casquete', ['Derecho','Izquierdo']]
         ], source: require('../assets/scheme/envolventes/2.png') },
     { id: '3', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F'], [1, 2, 3, 4, 5, 6]], 
-        ['Casquete', ['Inferior','Superior']]
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F'], [1, 2, 3, 4]], 
+        ['Casquete', ['Derecho','Izquierdo']]
         ], source: require('../assets/scheme/envolventes/3.png') },
     { id: '4', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F'], [1, 2, 3, 4]], 
-        ['Casquete', ['Inferior','Superior']]
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5, 6, 7]], 
+        ['Casquete',['Derecho','Izquierdo']]
         ], source: require('../assets/scheme/envolventes/4.png') },
     { id: '5', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5, 6]], 
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5]], 
         ['Casquete', ['Inferior','Superior']]
         ], source: require('../assets/scheme/envolventes/5.png') },
     { id: '6', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5, 6]], 
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F'], [1, 2, 3, 4, 5]], 
         ['Casquete', ['Inferior','Superior']]
         ], source: require('../assets/scheme/envolventes/5.png') },
     { id: '7', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4]], 
-        ['Casquete', ['Derecho','Izquierdo']]
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5, 6, 7]], 
+        ['Casquete', ['Inferior','Superior']]
         ], source: require('../assets/scheme/envolventes/7.png') },
     { id: '8', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F'], [1, 2, 3, 4, 5]], 
-        ['Casquete', ['Derecho','Izquierdo']]
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F'], [1, 2, 3, 4]], 
+        ['Casquete', ['Inferior','Superior']]
         ], source: require('../assets/scheme/envolventes/8.png') },
     { id: '9', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5, 6, 7]], 
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F'], [1, 2, 3, 4, 5, 6]], 
         ['Casquete', ['Inferior','Superior']]
         ], source: require('../assets/scheme/envolventes/9.png') },
     { id: '10', grid:[
@@ -65,25 +65,17 @@ const envolventes = [
         ['Casquete', ['Inferior','Superior']]
         ], source: require('../assets/scheme/envolventes/10.png') },
     { id: '11', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5]], 
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5, 6]], 
         ['Casquete', ['Inferior','Superior']]
         ], source: require('../assets/scheme/envolventes/11.png') },
     { id: '12', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F'], [1, 2, 3, 4, 5, 6, 7]], 
+        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4]], 
         ['Casquete', ['Inferior','Superior']]
-        ], source: require('../assets/scheme/envolventes/12.png') },
-    { id: '13', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], [1, 2, 3, 4, 5, 6, 7]], 
-        ['Casquete', ['Derecho','Izquierdo']]
-        ], source: require('../assets/scheme/envolventes/13.png') },
-    { id: '14', grid:[
-        ['Envolvente', ['A', 'B', 'C', 'D', 'E', 'F'], [1, 2, 3, 4]], 
-        ['Casquete', ['Derecho','Izquierdo']]
-        ], source: require('../assets/scheme/envolventes/14.png') },
+        ], source: require('../assets/scheme/envolventes/12.png') },  
 ];
 
-const casquetes = [
-    { id: '1', grid:[1, 2, 3, 4, 5], source: require('../assets/scheme/casquetes/1.png') },
+let casquetes = [
+    { id: '1', grid:[1, 2, 3, 4, 5], source: require('../assets/scheme/casquetes/1.png')},
     { id: '2', grid:[1, 2, 3, 4, 5, 6, 7, 8, 9], source: require('../assets/scheme/casquetes/2.png') },
     { id: '3', grid:[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], source: require('../assets/scheme/casquetes/3.png') },
 ];
